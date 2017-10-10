7ae45bd94a3977487c9b4176197584ce3f3a23c2
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-10-10 18:28:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -17,8 +17,9 @@
  end
 
   if (int % 3 == 0) && (int % 5 == 0) == 0  # if the number int is divisible by 3 and 5
-    return "FizzBuzz" # Go FizzBuzz
-    "FizzBuzz"
+    fizz_15 = "Fizzbuzz"
+    return fizz_15 # Go FizzBuzz
+    
   # elseif int % 4 == 0
   #   return ""
   end
