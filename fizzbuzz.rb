59d0bfa2a15107c1b4ebe8e0a7f74de0a6d789f0
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-10-10 20:18:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,21 @@
-#define method
-
-def fizzbuzz(int)
-
-  if (int % 3 == 0) && int < 15 # if the number int is divisible by 3
-    "Fizz"
-
-  end
-
-  if (int % 5 == 0) && int < 15 # if the number int is divisible by 5
-    "Buzz"
-
-  end
-
-  if (int % 3 && int % 5) == 0 # if the number int is divisible by 3 and 5
-    "FizzBuzz"
-  end
-end
+# #define method
+# 
+# def fizzbuzz(int)
+# 
+#   if int % 3 == 0 # if the number int is divisible by 3
+#     "Fizz"
+# 
+#   end
+# 
+#   if int % 5 == 0 # if the number int is divisible by 5
+#     "Buzz"
+# 
+#   end
+# 
+#   if (int % 3 && int % 5) == 0 # if the number int is divisible by 3 and 5
+#     "FizzBuzz"
+#   end
+# end
 
 
 #
@@ -37,19 +37,19 @@ end
 # end
 #
 #
-# def fizzbuzz(int)
-#
-#   if (int % 3 == 0) && int < 15 # if the number int is divisible by 3
-#     return "Fizz"
-#
-#   end
-#
-#   if (int % 5 == 0) && int < 15 # if the number int is divisible by 5
-#     return "Buzz"
-#
-#   end
-#
-#   if (int % 3 && int % 5) == 0 # if the number int is divisible by 3 and 5
-#     return "FizzBuzz"
-#   end
-# end
+def fizzbuzz(int)
+
+  if (int % 3 == 0) && int < 15 # if the number int is divisible by 3
+    return "Fizz"
+
+  end
+
+  if (int % 5 == 0) && int < 15 # if the number int is divisible by 5
+    return "Buzz"
+
+  end
+
+  if (int % 3 && int % 5) == 0 # if the number int is divisible by 3 and 5
+    return "FizzBuzz"
+  end
+end
