39d4d2c33fc92565d0746573e0c93311ebca068f
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-10-10 18:05:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,14 @@
 #define method
 
 def fizzbuzz(int)
-  fizzbuzz = ""
   if int % 3 == 0 # if the number int is divisible by 3
-    fizzbuzz = "Fizz" # Go fizz
+    return "Fizz" # Go fizz
+  end
 
   if int % 5 == 0 # if the number int is divisible by 5
-    fizzbuzz = "Buzz" # Go Buzz
+    return "Buzz" # Go Buzz
+ end
 
-  if fizzbuzz
-    print "Fizz"
   if (int % 5 && int % 5) == 0  # if the number int is divisible by 3 and 5
     return "FizzBuzz" # Go FizzBuzz
   # elseif int % 4 == 0
@@ -17,25 +16,18 @@ def fizzbuzz(int)
   end
 end
 
-
-# def fizzbuzz(int)
-#   fizzbuzz = ""
-#   if int % 3 == 0 # if the number int is divisible by 3
-#     fizzbuzz = "Fizz" # Go fizz
-#
-#   if int % 5 == 0 # if the number int is divisible by 5
-#     fizzbuzz = "Buzz" # Go Buzz
-#
-#   if fizzbuzz
-#     print "Fizz"
-#   if (int % 5 && int % 5) == 0  # if the number int is divisible by 3 and 5
-#     return "FizzBuzz" # Go FizzBuzz
-#   # elseif int % 4 == 0
-#   #   return ""
-#   end
-# end
-
 # fizzbuzz(3) # => You should see a return of "Fizz"
 # fizzbuzz(5) # => You should see a return of nil
 # fizzbuzz(15)
 # fizzbuzz(4)
+
+
+# def fizzbuzz(int)
+#   if int % 3 == 0 # if the number int is divisible by 3
+#     puts "Fizz" # Go fizz
+#   end
+# end
+#
+# fizzbuzz(3) # => You should see a return of "Fizz"
+# fizzbuzz(5) # => You should see a return of nil
+# fizzbuzz()  # => You should get an ArgumentError
