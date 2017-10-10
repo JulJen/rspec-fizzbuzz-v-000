576ac7df183d83c522a6f9c109f47e401cf535ee
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-10-10 19:01:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,17 +5,14 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     return "Fizz"
     # return fizz_3 # Go fizz
-  end
 
   if int % 5 == 0 # if the number int is divisible by 5
     return "Buzz"
     # return fizz_5 # Go Buzz
-  end
 
   if (int % 3 == 0) && (int % 5 == 0 # if the number int is divisible by 3 and 5
     return "Fizzbuzz"
     # return fizz_15 # Go FizzBuzz
-  end
 end
 
 # fizzbuzz(3) # => You should see a return of "Fizz"
