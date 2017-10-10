ab450f7742f6f85b64f08106e9fdad897dc30988
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-10-10 19:39:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,17 @@
 # #define method
-# 
+#
 # def fizzbuzz(int)
-# 
+#
 #   if (int % 3 == 0) && int < 15 # if the number int is divisible by 3
 #     "Fizz"
 #     # return fizz_3 # Go fizz
 #   end
-# 
+#
 #   if (int % 5 == 0) && int < 15 # if the number int is divisible by 5
 #     "Buzz"
 #     # return fizz_5 # Go Buzz
 #   end
-# 
+#
 #   if (int % 3 && int % 5) == 0 # if the number int is divisible by 3 and 5
 #     "FizzBuzz"
 #   end
