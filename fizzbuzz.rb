703c912f8b78cb40e4b5a01d629f2bbb1771e75c
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-10-10 20:24:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -39,7 +39,7 @@
 #
 def fizzbuzz(int)
 
-  if (int % 3 && int % 5) == 0 # if the number int is divisible by 3 and 5
+  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5
     return "FizzBuzz"
   end
 
